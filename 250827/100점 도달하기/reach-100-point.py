936f708f1f97509b55332b65737f0e9bd212539d
woodzverse <woodzverse@naver.com>
2025-08-27 02:47:27 +0900

    Title: 100점 도달하기, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250827/100점 도달하기/reach-100-point.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+for i in range(N, 101):
+    if i >= 90:
+        print("A", end=" ")
+    elif i >= 80:
+        print("B", end=" ")
+    elif i >= 70:
+        print("C", end=" ")
+    elif i >= 60:
+        print("D", end=" ")
+    else: 
+        print("F", end=" ")
