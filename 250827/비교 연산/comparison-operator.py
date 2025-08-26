3388d07001515da1475ca380822d334eb6cb7ad7
woodzverse <woodzverse@naver.com>
2025-08-27 02:20:06 +0900

    Title: 비교 연산, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250827/비교 연산/comparison-operator.py
@@ -0,0 +1,8 @@
+A, B = map(int, input().split())
+
+print(int(bool(A >= B)))
+print(int(bool(A > B)))
+print(int(bool(A <= B)))
+print(int(bool(A < B)))
+print(int(bool(A == B)))
+print(int(bool(A != B)))
