8ec78b44bb8ac792261546a98d5d58c72c6b521c
woodzverse <woodzverse@naver.com>
2025-09-03 02:40:09 +0900

    Title: 배수의 개수, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250903/배수의 개수/number-of-multipliers.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+let cnt3 = 0, cnt5 = 0;
+
+for (let i = 0; i < input.length; i++) {
+    num = input[i];
+    if (num % 3 == 0) {
+        cnt3 += 1;
+    }
+    if (num % 5 == 0) {
+        cnt5 += 1;
+    }
+}
+console.log(cnt3, cnt5);
