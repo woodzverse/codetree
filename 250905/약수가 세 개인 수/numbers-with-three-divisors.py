86bf894cdd8560325320847ff42b1db4f6367a66
woodzverse <woodzverse@naver.com>
2025-09-05 11:28:22 +0900

    Title: 약수가 세 개인 수, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 250905/약수가 세 개인 수/numbers-with-three-divisors.py
+++ 250905/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -4,7 +4,7 @@ start, end = map(int, input().split())
 count = 0
 for i in range(start, end + 1):
     num = 0
-    for j in range(1, start + 1):
+    for j in range(1, i + 1):
         if i % j == 0:
             num += 1
     
